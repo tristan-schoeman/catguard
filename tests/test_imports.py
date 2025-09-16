0876ef78e4c3bc69f38413f80bb70a4c1cf9ea3d
Tristan Schoeman <tris.schoeman@gmail.com>
2025-09-15 20:02:43 -0600

    Add tests

---
+++ tests/test_imports.py
@@ -0,0 +1,7 @@
+
+def test_imports():
+    import src.main
+    from src.vision.motion_detector import ROIMotionDetector
+    from src.audio.scratch_detector import ScratchDetector, AudioConfig
+    from src.control.sound_player import SoundPlayer
+    from src.control.water_turret import WaterTurret
